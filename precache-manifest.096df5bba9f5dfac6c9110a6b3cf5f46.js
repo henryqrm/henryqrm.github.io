39046b20afea524e036f1aef66dc06f4e2effe7c
Henrique Rodrigues <henryqrm@gmail.com>
2019-03-11 15:43:44 -0300

    Updates

---
+++ precache-manifest.096df5bba9f5dfac6c9110a6b3cf5f46.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e2669b2b68c79bd4940",
+    "url": "/static/css/main.a72a567f.chunk.css"
+  },
+  {
+    "revision": "4e2669b2b68c79bd4940",
+    "url": "/static/js/main.4e2669b2.chunk.js"
+  },
+  {
+    "revision": "871397b8d1353aa7c425",
+    "url": "/static/css/1.0c2cc092.chunk.css"
+  },
+  {
+    "revision": "871397b8d1353aa7c425",
+    "url": "/static/js/1.871397b8.chunk.js"
+  },
+  {
+    "revision": "628ec480bc9487d40ada",
+    "url": "/static/js/2.628ec480.chunk.js"
+  },
+  {
+    "revision": "305330098ad220d4ba17",
+    "url": "/static/js/3.30533009.chunk.js"
+  },
+  {
+    "revision": "3ccd529cd49e23c5b2c9",
+    "url": "/static/css/4.b123a49d.chunk.css"
+  },
+  {
+    "revision": "3ccd529cd49e23c5b2c9",
+    "url": "/static/js/4.3ccd529c.chunk.js"
+  },
+  {
+    "revision": "eaa67ef805b3f90292a9",
+    "url": "/static/css/5.32a2c88f.chunk.css"
+  },
+  {
+    "revision": "eaa67ef805b3f90292a9",
+    "url": "/static/js/5.eaa67ef8.chunk.js"
+  },
+  {
+    "revision": "41e6a09e3cf315d1bc7e",
+    "url": "/static/js/6.41e6a09e.chunk.js"
+  },
+  {
+    "revision": "ce2b35c344e0a9fab826",
+    "url": "/static/js/runtime~main.ce2b35c3.js"
+  },
+  {
+    "revision": "0f2f9d7f113542e08f1710d64a3aa45d",
+    "url": "/static/media/navi-logo.0f2f9d7f.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/react-logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "eab7aef1d230e474d23d1a6f83ea91fc",
+    "url": "/index.html"
+  }
+];
